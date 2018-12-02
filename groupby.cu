6a61f0dce0b0c015070b1dc9e3ed2bd2b64389e5
Chris <patte539@umn.edu>
2018-12-01 20:48:00 -0600

    Change inputs

--- groupby.cu
+++ groupby.cu
@@ -4,64 +4,47 @@
 #include <thrust/device_vector.h>
 enum reduction_op { max, min, sum, count, mean };
 
-template<typename T>
-struct Groupby
-{
-    T* key_columns[];
-	int* key_starts;
-	int* key_counts;
-	int num_key_columns;
-	int num_key_rows;
-
-	T* value_columns[];
-	int* output_index[];
-	int num_value_columns;
-	int num_value_rows;
-
-	reduction_op ops[];
-	int num_ops;
-
-	T* output_keys;
-	T* output_values;
-	int num_output_rows;
-
-
-};
-
 //Launch reduction kernels for each column based on their specified operation
 template <typename T>
-void perform_operators(Groupby &input)
+void perform_operators(T* key_columns[], int num_key_columns, int num_key_rows,
+	T* value_columns[], int num_value_columns, int num_value_rows,
+	reduction_op ops[], int num_ops, T* output_keys[], T* output_values[])
 {
 	//TODO: hashing here
-	thrust::device_vector<T> d_hash_keys(input.num_key_rows);
+	thrust::device_vector<T> d_hash_keys(num_key_rows);
 
 	//create index array for sorting. 
-	thrust::device_vector<int> d_i(input.num_key_rows);
+	thrust::device_vector<int> d_i(num_key_rows);
 	thrust::sequence(thrust::host, d_i.begin(), d_i.end()); 
 
 	//sort by key, also sort values. The result can be used to sort the actual data arrays later
 	thrust::sort_by_key(d_hash_keys.begin(), d_hash_keys.end(), d_i);
 
 	//Find the position of first key for each group
-	thrust::device_vector<int> d_key_starts(input.num_key_rows);
+	thrust::device_vector<int> d_key_starts(num_key_rows);
 	thrust::sequence(d_key_starts.begin(), d_key_starts.end()); //this sequence represents the index for each key
 	int new_end  = (thrust::unique_by_key(d_hash_keys.begin(), d_hash_keys.end(), d_key_starts.begin())).first - d_hash_keys.begin();
 	//after unique_by_key, d_key_starts holds the start of each group.
 
 	//setup output arrays
-	input.num_output_rows = new_end;
-	input.output_keys = new T[num_output_rows];
-	input.output_values = new T[num_output_rows*input.num_value_columns];
+	int num_output_rows = new_end;
+	output_keys = new T[num_output_rows];
+	output_values = new T[num_output_rows*num_value_columns];
 
-	for (int i = 0; i<input.num_ops, i++){//i represents column of output
+	//copy back unique keys
+	thrust::copy(d_hash_keys.begin(), d_hash_keys.begin() + new_end, output_keys;
+
+
+	//iterate though all columns of the matrix. Perfrom the operation corresponding to that column
+	for (int i = 0; i<num_ops, i++){//i represents column of output
 		//get this column of data. copy does [first, last) 
-		int start = input.value_columns.begin() + i*input.num_value_rows;
-		int end = input.value_columns.begin() + (i+1)*input.num_value_rows;
-		thrust::device_vector<T> col(input.num_value_rows), sorted_col(input.num_value_rows);
-		thrust::copy(input.value_columns.begin() + start, input.value_columns.begin() + end,col.begin());
+		int start = i*num_value_rows;
+		int end = (i+1)*num_value_rows;
+		thrust::device_vector<T> col(num_value_rows), sorted_col(num_value_rows);
+		thrust::copy(value_columns + start, value_columns + end,col.begin());
 
 		//the column is not sorted yet so use d_i to sort!
-		thrust::copy_n(thrust::make_permutation_iterator(col.begin(), d_i.begin()), input.num_value_rows, sorted_col.begin());
+		thrust::copy_n(thrust::make_permutation_iterator(col.begin(), d_i.begin()), num_value_rows, sorted_col.begin());
 		for (int j = 0; j < new_end; j++){ //iterate over the groups of keys... j = output row
 			int start = d_key_starts[j];
 			int end;
