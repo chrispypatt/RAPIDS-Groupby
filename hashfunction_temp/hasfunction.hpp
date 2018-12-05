887f1151520eb0254e0e6f0576f90ba1f7354d08
Maggie8888 <45131108+Maggie8888@users.noreply.github.com>
2018-12-04 22:28:59 -0600

    Add files via upload

---
+++ hashfunction_temp/hasfunction.hpp
@@ -0,0 +1,13 @@
+//
+//  hasfunction.hpp
+//  
+//
+//  Created by menglu liang on 12/4/18.
+//
+
+#ifndef hasfunction_hpp
+#define hasfunction_hpp
+
+#include <stdio.h>
+
+#endif /* hasfunction_hpp */
---
+++ hashfunction_temp/rehashfunction.hpp
@@ -0,0 +1,13 @@
+//
+//  rehashfunction.hpp
+//  
+//
+//  Created by menglu liang on 12/4/18.
+//
+
+#ifndef rehashfunction_hpp
+#define rehashfunction_hpp
+
+#include <stdio.h>
+
+#endif /* rehashfunction_hpp */
