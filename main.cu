641ee77e8a7a41d9768dc2f898e6573a54bd1f61
Tonglin Chen <chen5202@umn.edu>
2018-12-17 15:12:07 -0600

    pinned input memory

--- main.cu
+++ main.cu
@@ -35,8 +35,10 @@ int main(int argc, const char * argv[]) {
 
         slowGroupby.fillRand(num_distinct_keys, num_rows);
 
-        int *original_key_columns = new int[num_key_cols*num_rows];
-        int *original_value_columns = new int[num_val_cols*num_rows];
+        int *original_key_columns;
+	cudaMallocHost((void**)&original_key_columns, sizeof(int)*num_key_cols*num_rows);
+        int *original_value_columns;
+	cudaMallocHost((void**)&original_value_columns, sizeof(int)*num_val_cols*num_rows);
         std::copy(slowGroupby.key_columns, slowGroupby.key_columns + num_key_cols*num_rows, original_key_columns);
         std::copy(slowGroupby.value_columns, slowGroupby.value_columns + num_val_cols*num_rows, original_value_columns);
         
@@ -70,7 +72,7 @@ int main(int argc, const char * argv[]) {
 
         slowGroupby.validGPUResult(gpu_output_keys, gpu_output_values, gpu_output_rows);
 
-        delete [] original_value_columns;
-        delete [] original_key_columns;
+        cudaFreeHost(original_value_columns);
+        cudaFreeHost(original_key_columns);
         return 0;
 }
